2d3035cb0d055b195505f77956835ec9d695839a
KahlenHernani <Kahlenhernani@gmail.com>
2026-03-28 12:24:43 -0400

    MAde website

---
+++ FloodNet/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div id="left">
+          <h1>FloodNet</h1>
+          <p>
+            Hey
+          </p>
+        </div>
+      </section>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
